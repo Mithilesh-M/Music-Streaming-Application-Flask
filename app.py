6cf5e4baf30960cd0aa3869605adc19fd0aad2a4
Mithilesh-M <msmithilesh@yahoo.in>
2021-07-14 12:30:14 +0530

    Add database with respective fields for app

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
 import os
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +12,29 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300))
+    date_created = db.Column(db.DateTime,  default=datetime.now(pytz.timezone('Asia/Kolkata')))
+
+    def __repr__(self):
+        return self.title
+
+
+class Music(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300))
+    artist = db.Column(db.String(300))
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'))
+    filename = db.Column(db.String(300))
+    date_created = db.Column(db.DateTime,  default=datetime.now(pytz.timezone('Asia/Kolkata')))
+
+    def __repr__(self):
+        return self.title
+
 
 if __name__ == "__main__":
     app.run(debug=True)
